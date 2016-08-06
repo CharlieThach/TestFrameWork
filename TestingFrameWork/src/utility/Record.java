76148a0d6e3192713235b2f5ca02de5e800fd686
Charles <charliethach@csus.edu>
2016-08-05 17:00:47 -0700

    update and adding jxl-2.6(jexcel) and util files into repository.

---
+++ TestingFrameWork/src/utility/Record.java
@@ -0,0 +1,39 @@
+/*
+ * Purpose of this class is to record all the data from excel
+ * there are setter/getter
+ */
+
+package utility;
+public class Record {
+	private String testID;
+	private String user;
+	private String pass; 
+	private String expectVal;
+	
+	public String getMethodName(){
+		return testID; 
+	}
+	public String getUser(){
+		return user;
+	}
+	public String getPass(){
+		return pass;
+	}
+	public String getExpectedValue(){
+		return expectVal;
+	}
+	
+	//set setter
+	public void setTestID(String id){
+		this.testID = id;
+	}
+	public void setUser(String name){
+		this.user = name;
+	}
+	public void setPass(String auth){
+		this.pass = auth; 
+	}
+	public void setExpectedValue(String val){
+		this.expectVal = val; 
+	}
+}
---
+++ TestingFrameWork/src/utility/loadTestCases.java
@@ -0,0 +1,14 @@
+package utility;
+
+import java.io.BufferedReader;
+
+public class loadTestCases {
+	
+	String filePath;
+	
+
+	public void readExcel(){
+		BufferedReader read = new BufferedReader(); 
+	
+	}
+}
